c962fe51538328ce10782a478a09713d5e1f31cb
klipar <bhdnkarpenko123@gmail.com>
2025-07-22 13:50:02 +0300

    add file fore creating telegram users database

---
+++ createTelegramUsersDatabase.py
@@ -0,0 +1,20 @@
+from easy import Config
+import sqlite3
+
+
+config = Config(configPath="Configs/telegramBot.json",
+                onFailedToLoadConfig=lambda: exit(0))
+
+conn = sqlite3.connect(config.getValue("Users database"))
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    TelegramID INTEGER PRIMARY KEY,
+    subscribed INTEGER NOT NULL,
+    sudo INTEGER NOT NULL
+)
+''')
+
+conn.commit()
+conn.close()
